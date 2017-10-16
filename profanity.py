320e0ca11766fecb4fbad823a7c8ce1b2fc31c85
Ash <ash@lap.domain.name>
2017-10-16 20:47:48 +0530

    initiate twitter

--- profanity.py
+++ profanity.py
@@ -17,10 +17,11 @@ import datetime
 import json
 import time
 import tweepy
+import config
+import random
 
-def getCommits(word_list):
+def getCommits(word_list, now):
 	today = datetime.date.today()
-	now = datetime.datetime.now()
 	print("Now:"+ str(today))
 	i = 0
 	for word in word_list:
@@ -32,7 +33,7 @@ def getCommits(word_list):
 		i +=1
 
 		if i == 9 or i == 18:
-			print("sleeping")
+			print("sleeping") # Sleep one minute to avoid API rejection by Github (status 403)
 			time.sleep(60)
 
 		for num in range(len(x['items'])):
@@ -42,24 +43,40 @@ def getCommits(word_list):
 				if 10<len(msg) and len(msg)<141:
 					if "\n" or "\n\n" in msg:
 						msg.replace("\n","")
-						msg.replace("\n\n","")
-						print(msg)
+						msg.replace("\n\n","")  ##TODO
+						#print(msg)
 					f.writelines(msg+ "\n")
 	return
 
-def tweet():
-	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-	auth.set_access_token(access_token, access_token_secret)
+def tweet(now):
+	print("Ready to tweet")
+	auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+	auth.set_access_token(config.access_token, config.access_token_secret)
 
 	api = tweepy.API(auth)
+	# If the authentication was successful, you should see the name of the account print out
+
+	#print(api.me().name)
+	
+	commit_msg = []
+	with open("{}.txt".format(now), 'r') as f:
+		commit_msg = f.read().split('\n')
+	
+	print(random.choice(commit_msg))
+
+	status = api.update_status(status= random.choice(commit_msg))
+	print (status.id) # print id if ts tweeted 
+
+	return
 
 
 
 def main():
+	now = datetime.datetime.now()
 	
 	word_list = ["fuck", "bitch", "shit", "tits", "asshole", "arsehole", "cocksucker", "cunt", "hell", "douche", " testicle", " twat", " bastard", " sperm", " dildo", " wanker", "prick", "penis", "vagina", "whore", "boner", "suck"]
-	getCommits(word_list)
-	#tweet()
+	getCommits(word_list, now)
+	tweet(now)
 
 
 if __name__ == '__main__':
