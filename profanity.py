d4d0765d50713dbd5e7d0f352ceb606adda6d5cc
Ashwin <thatsashwin@gmail.com>
2017-10-29 05:21:58 +0530

    Duplicate commits removal

--- profanity.py
+++ profanity.py
@@ -20,8 +20,7 @@ import tweepy
 import config
 import random
 
-def getCommits(word_list, now):
-	today = datetime.date.today()
+def getCommits(word_list, now, today):
 	print("Now:"+ str(today))
 	i = 0
 	for word in word_list:
@@ -29,7 +28,7 @@ def getCommits(word_list, now):
 		headers={'accept':'application/vnd.github.cloak-preview'}
 		r = requests.get(url, headers=headers)
 		x = json.loads(r.text)
-		#print(r.status_code)
+		print(r.text)
 		i +=1
 
 		if i == 9 or i == 18:
@@ -37,35 +36,45 @@ def getCommits(word_list, now):
 			time.sleep(60)
 
 		for num in range(len(x['items'])):
+			print("In")
 			with open("{}.txt".format(now), 'a') as f:
 				msg = x['items'][num]['commit']['message']
 				msg.strip()
+				print("msg")
 				if 10<len(msg) and len(msg)<141:
 					if "\n" or "\n\n" in msg:
 						msg.replace("\n","")
-						msg.replace("\n\n","")  ##TODO
-						#print(msg)
-					f.writelines(msg+ "\n")
+						msg.replace("\n\n","")  ##TODO fix new line bug
+						print(msg)
+					f.writelines(msg+ "\n") # Write all relevent commit msgs to a file
 	return
 
-def tweet(now):
+def tweet(now, today):
 	print("Ready to tweet")
+	Found = False
 	auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
 	auth.set_access_token(config.access_token, config.access_token_secret)
 
 	api = tweepy.API(auth)
-	# If the authentication was successful, you should see the name of the account print out
+	
 
-	#print(api.me().name)
+	#print(api.me().name) #If the authentication was successful, you should see the name of the account print out
 	
 	commit_msg = []
-	with open("{}.txt".format(now), 'r') as f:
+	with open("commits/{}.txt".format(now), 'r') as f:
 		commit_msg = f.read().split('\n')
 	
-	print(random.choice(commit_msg))
 
-	status = api.update_status(status= random.choice(commit_msg))
-	print (status.id) # print id if ts tweeted 
+	while (Found == False):
+		draft = random.choice(commit_msg)
+		print(draft)
+		with open("drafts/{}.txt".format(today), 'a') as f:
+			past_tweets_today = f.read().split('\n')
+			if not past_tweets_today == draft:
+				f.writelines(draft+ "\n")
+				status = api.update_status(status= draft)
+				print (status.id) # print id if ts tweeted 
+				Found = True
 
 	return
 
@@ -73,10 +82,11 @@ def tweet(now):
 
 def main():
 	now = datetime.datetime.now()
+	today = datetime.date.today()
 	
 	word_list = ["fuck", "bitch", "shit", "tits", "asshole", "arsehole", "cocksucker", "cunt", "hell", "douche", " testicle", " twat", " bastard", " sperm", " dildo", " wanker", "prick", "penis", "vagina", "whore", "boner", "suck"]
-	getCommits(word_list, now)
-	tweet(now)
+	getCommits(word_list, now, today)
+	tweet(now, today)
 
 
 if __name__ == '__main__':
